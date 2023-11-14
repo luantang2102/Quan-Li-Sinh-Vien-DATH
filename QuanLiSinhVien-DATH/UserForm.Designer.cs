c9d8e682869251686c94c47300255c2606a42e29
LuanTang <luantang.work@gmail.com>
2023-11-14 21:34:09 +0700

    finished login function

---
+++ QuanLiSinhVien-DATH/UserForm.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace QuanLiSinhVien_DATH
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signInPanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.signUpSwitchBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.signUpPanel = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.password2RTB = new System.Windows.Forms.TextBox();
+            this.signInSwitchBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password1RTB = new System.Windows.Forms.TextBox();
+            this.usernameRTB = new System.Windows.Forms.TextBox();
+            this.signUpBtn = new System.Windows.Forms.Button();
+            this.signInPanel.SuspendLayout();
+            this.signUpPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // signInPanel
+            // 
+            this.signInPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.signInPanel.Controls.Add(this.label4);
+            this.signInPanel.Controls.Add(this.label9);
+            this.signInPanel.Controls.Add(this.label8);
+            this.signInPanel.Controls.Add(this.signUpSwitchBtn);
+            this.signInPanel.Controls.Add(this.label1);
+            this.signInPanel.Controls.Add(this.passwordTB);
+            this.signInPanel.Controls.Add(this.usernameTB);
+            this.signInPanel.Controls.Add(this.loginBtn);
+            this.signInPanel.Location = new System.Drawing.Point(114, 0);
+            this.signInPanel.Name = "signInPanel";
+            this.signInPanel.Size = new System.Drawing.Size(495, 449);
+            this.signInPanel.TabIndex = 0;
+            this.signInPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.signInPanel_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(138, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 42);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Đăng Nhập";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(130, 183);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Mật khẩu :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(130, 136);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Tên tài khoản :";
+            // 
+            // signUpSwitchBtn
+            // 
+            this.signUpSwitchBtn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.signUpSwitchBtn.Location = new System.Drawing.Point(269, 323);
+            this.signUpSwitchBtn.Name = "signUpSwitchBtn";
+            this.signUpSwitchBtn.Size = new System.Drawing.Size(93, 27);
+            this.signUpSwitchBtn.TabIndex = 4;
+            this.signUpSwitchBtn.Text = "Đăng ký";
+            this.signUpSwitchBtn.UseVisualStyleBackColor = false;
+            this.signUpSwitchBtn.Click += new System.EventHandler(this.signUpSwitchBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Không có tài khoản? ";
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(133, 202);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(229, 22);
+            this.passwordTB.TabIndex = 2;
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Location = new System.Drawing.Point(133, 155);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(229, 22);
+            this.usernameTB.TabIndex = 1;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.loginBtn.Location = new System.Drawing.Point(133, 243);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(229, 64);
+            this.loginBtn.TabIndex = 0;
+            this.loginBtn.Text = "ĐĂNG NHẬP";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // signUpPanel
+            // 
+            this.signUpPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.signUpPanel.Controls.Add(this.label7);
+            this.signUpPanel.Controls.Add(this.label6);
+            this.signUpPanel.Controls.Add(this.label5);
+            this.signUpPanel.Controls.Add(this.label3);
+            this.signUpPanel.Controls.Add(this.password2RTB);
+            this.signUpPanel.Controls.Add(this.signInSwitchBtn);
+            this.signUpPanel.Controls.Add(this.label2);
+            this.signUpPanel.Controls.Add(this.password1RTB);
+            this.signUpPanel.Controls.Add(this.usernameRTB);
+            this.signUpPanel.Controls.Add(this.signUpBtn);
+            this.signUpPanel.Location = new System.Drawing.Point(114, 0);
+            this.signUpPanel.Name = "signUpPanel";
+            this.signUpPanel.Size = new System.Drawing.Size(492, 450);
+            this.signUpPanel.TabIndex = 5;
+            this.signUpPanel.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(133, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Nhập lại mật khẩu :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(130, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Mật khẩu :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(130, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tên tài khoản :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(161, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 42);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Đăng Ký";
+            // 
+            // password2RTB
+            // 
+            this.password2RTB.Location = new System.Drawing.Point(133, 230);
+            this.password2RTB.Name = "password2RTB";
+            this.password2RTB.Size = new System.Drawing.Size(229, 22);
+            this.password2RTB.TabIndex = 10;
+            // 
+            // signInSwitchBtn
+            // 
+            this.signInSwitchBtn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.signInSwitchBtn.Location = new System.Drawing.Point(247, 347);
+            this.signInSwitchBtn.Name = "signInSwitchBtn";
+            this.signInSwitchBtn.Size = new System.Drawing.Size(115, 27);
+            this.signInSwitchBtn.TabIndex = 9;
+            this.signInSwitchBtn.Text = "Đăng nhập";
+            this.signInSwitchBtn.UseVisualStyleBackColor = false;
+            this.signInSwitchBtn.Click += new System.EventHandler(this.signInSwitchBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Đã có tài khoản? ";
+            // 
+            // password1RTB
+            // 
+            this.password1RTB.Location = new System.Drawing.Point(133, 178);
+            this.password1RTB.Name = "password1RTB";
+            this.password1RTB.Size = new System.Drawing.Size(229, 22);
+            this.password1RTB.TabIndex = 7;
+            // 
+            // usernameRTB
+            // 
+            this.usernameRTB.Location = new System.Drawing.Point(133, 128);
+            this.usernameRTB.Name = "usernameRTB";
+            this.usernameRTB.Size = new System.Drawing.Size(229, 22);
+            this.usernameRTB.TabIndex = 6;
+            // 
+            // signUpBtn
+            // 
+            this.signUpBtn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.signUpBtn.Location = new System.Drawing.Point(133, 267);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(229, 64);
+            this.signUpBtn.TabIndex = 5;
+            this.signUpBtn.Text = "ĐĂNG KÝ";
+            this.signUpBtn.UseVisualStyleBackColor = false;
+            this.signUpBtn.Click += new System.EventHandler(this.signUpBtn_Click);
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(608, 448);
+            this.Controls.Add(this.signUpPanel);
+            this.Controls.Add(this.signInPanel);
+            this.Name = "UserForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.UserForm_Load);
+            this.signInPanel.ResumeLayout(false);
+            this.signInPanel.PerformLayout();
+            this.signUpPanel.ResumeLayout(false);
+            this.signUpPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel signInPanel;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button signUpSwitchBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel signUpPanel;
+        private System.Windows.Forms.Button signInSwitchBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password1RTB;
+        private System.Windows.Forms.TextBox usernameRTB;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.TextBox password2RTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
